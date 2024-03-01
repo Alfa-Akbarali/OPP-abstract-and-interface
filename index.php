33c1725b1c3f59d7a57905694494d4338da2cf63
Alfa_001 <akbaralidata01@gmail.com>
2024-03-01 10:14:20 +0500

    commited

--- index.php
+++ index.php
@@ -4,24 +4,32 @@ interface Animal
 {
     public function makeAnimal();
 }
-  
+  trait Make 
+  {
+    protected $type;
+    public function __construct($type) {
+      $this->type = $type;
+    }
+  }
   class Cat implements Animal 
   {
-    public function makeAnimal() 
+    use Make;
+    public function makeAnimal()
     {
-      echo "animal: cat\n";
+      return "This is a {$this->type} animal.";
     }
   }
   class Dog implements Animal 
   {
-    public function makeAnimal() 
+    use Make;
+    public function makeAnimal()
     {
-      echo "animal: dog\n";
+      return "This is a {$this->type} animal.";
     }
   }
 
-  $cat = new Cat();
-  $dog = new Dog();
+  $cat = new Cat("Cat");
+  $dog = new Dog("Dog");
   $animals = array($cat, $dog);
 
   foreach($animals as $animal) 
@@ -32,29 +40,29 @@ interface Animal
 
 
     //----abstract class
-    abstract class Color 
-    {
-        public $name;
-        public function __construct($name)
-        {
-            $this->name = $name;
-        }
-        abstract public function intro() : string ;
-    }
-    class White extends Color
-    {
-        public function intro() : string{
-            return "Your color is $this->name!";
-        }
-    } 
-    class Blue extends Color 
-    {
-        public function intro() : string
-        {
-            return "Your color is $this->name!";
-        }
-    }
-    $blue = new Blue("blue");
-    $white = new White("white");
-    echo $white->intro() . PHP_EOL;
-    echo $blue->intro() . PHP_EOL;
+    // abstract class Color 
+    // {
+    //     public $name;
+    //     public function __construct($name)
+    //     {
+    //         $this->name = $name;
+    //     }
+    //     abstract public function intro() : string ;
+    // }
+    // class White extends Color
+    // {
+    //     public function intro() : string{
+    //         return "Your color is $this->name!";
+    //     }
+    // } 
+    // class Blue extends Color 
+    // {
+    //     public function intro() : string
+    //     {
+    //         return "Your color is $this->name!";
+    //     }
+    // }
+    // $blue = new Blue("blue");
+    // $white = new White("white");
+    // echo $white->intro() . PHP_EOL;
+    // echo $blue->intro() . PHP_EOL;
--- main.php
+++ main.php
@@ -1,5 +1,4 @@
 <?php 
-
     abstract class Vehicle 
     {
         protected $type;
@@ -10,7 +9,7 @@
         }
         abstract public function info();
     }
-    class Car extends Vehicle
+    trait Constructable
     {
         protected $model;
         public function __construct($type, $model)
@@ -18,18 +17,39 @@
             parent::__construct($type);
             $this->model = $model;
         }
+        
+    }
+    trait Printer
+    {
         public function info()
         {
-            return "Type: {$this->type} Model: {$this->model}\n";
+            return "Type: {$this->type}\nModel: {$this->model}\n";
         }
+    }
+    class Car extends Vehicle
+    {
+        use Constructable, Printer;
     } 
     class Buss extends Car 
     {
         protected $model;
-        public function info()
-        {
-            return "Type: {$this->type};\nModel: {$this->model};\n";
-        }
+        use Printer;
     }
     $buss = new Buss("Buss","Car");
     echo $buss->info(). PHP_EOL;
+
+
+    // interface Main 
+    // {
+    //     public function add();
+    // }
+    // class Airplane implements Main 
+    // {
+    //   public function add() 
+    //   {
+    //     echo "Vehicle: Airplane\n";
+    //   }
+    // }
+    // $main = new Airplane();
+    // $main->add();
+    
